ec588ec90ddb638349dae50642fd0ae009b5c55b
saehan-choi <gmlssns5859@naver.com>
2022-05-12 16:02:54 +0900

    test

--- new_representation_learning.py
+++ new_representation_learning.py
@@ -13,7 +13,7 @@ from glob import glob
 import cv2
 import numpy as np
 
-from sklearn.neighbors import NearestNeighbors
+import timm
 
 # 간단한 네트워크부터 끝내고, 그 다음시작 ㄱㄱ
 
@@ -29,16 +29,13 @@ class CFG:
     # 이것만 있으면 image의 사이즈를 줄였다가, 또 늘릴수 있음 같은크기로 ( list(reversed()) 를 통해서 )
     in_channels = [3, 64, 128, 256, 512]
     # 이거 네트워크 더 깊게하니깐 maxpooling 때문에 에러가 납니다. -> 추후 수정 부탁드립니다. .__. 
-    out_channels = [64, 128, 256, 512, 1024]
-
+    out_channels = [64, 128, 256, 512, 1280]
     img_resize = (224, 224)
-
     device = 'cuda'
-
-    lr = 3e-3
+    lr = 3e-4
     loss = nn.MSELoss()
     epochs = 10
-    batch_size = 64
+    batch_size = 32
     # batch_size = 128
 
     transform = A.Compose([
@@ -49,18 +46,12 @@ class CFG:
 class encoderModel(nn.Module):
     def __init__(self):
         super(encoderModel, self).__init__()
-        self.model = nn.ModuleList(self.encoderSequential(in_channel, out_channel) for in_channel, out_channel in zip(CFG.in_channels, CFG.out_channels))
-    def encoderSequential(self, in_channels, out_channels):
-        return nn.Sequential(
-                                nn.Conv2d(in_channels, out_channels, kernel_size=(3,3), padding=(1,1)),
-                                nn.ReLU(inplace=True),
-                                # inplace True하면 본래 input값을 수정하겠단 말임 -> 따라서 메모리 사용량이 좋아짐.
-                                nn.MaxPool2d((2,2))
-                            )
+        self.model = timm.create_model('efficientnet_b0')
+        self.model.global_pool = torch.nn.Identity()
+        self.model.classifier = torch.nn.Identity()
+
     def forward(self, x):
-        for model in self.model:
-            output = model(x)
-            x = output
+        output = self.model(x)
         return output
 
 
@@ -73,6 +64,7 @@ class decoderModel(nn.Module):
     def decoderSequential(self, in_channels, out_channels):
         return nn.Sequential(
                                 nn.ConvTranspose2d(in_channels, out_channels, kernel_size=(2,2), stride=(2,2)),
+                                nn.BatchNorm2d(out_channels),
                                 nn.ReLU(inplace=True),
                             )
 
@@ -169,45 +161,9 @@ def val_one_epoch(encoder, decoder, dataloader, epoch, loss_fn, device):
             torch.save(encoder.state_dict(), CFG.weightsavePath+"encoder_model.pt")
             torch.save(decoder.state_dict(), CFG.weightsavePath+"decoder_model.pt")
 
-
-# https://velog.io/@ppyooy336/Numpy%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%98%EC%97%AC-KNN-%EA%B5%AC%ED%98%84
-# 여기참조하고 https://github.com/public-ai/dl-lecture/blob/master/ml-homework/1_Numpy%EB%A5%BC%20%ED%99%9C%EC%9A%A9%ED%95%9C%20%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D%20-%20KNN%20%EB%B6%84%EB%A5%98%EA%B8%B0.ipynb
-# 이사람이 배운곳
-
-def nearest_neighbor(image, all_dataset, labels, num_sample):
-    # num_sample개의 이미지 path만 추출
-
-    # 정규화 + 후에 np.sqrt(k^2)
-    # 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
-    square = (all_dataset-image)**2
-
-    distance = np.sqrt(square.sum(axis=1))
-    # print(distance)
-    # print(distance.shape)
-
-    sorted_idx = np.argsort(distance)[:num_sample]
-    # 제일작은 index를 반환함
-
-    for i in sorted_idx:
-        similarImgPath = labels[i]
-        print(similarImgPath)
-
-
-
-# def k_nearest_neighbor(image, all_dataset, labels, K):
-#     # 정규화 + 후에 np.sqrt(k^2)
-#     # 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
-#     square = (all_d       ataset-image)**2
-
-#     distance = np.sqrt(square.sum(axis=1))
-
-#     similar = None
-#     # 필요시 axis 바꾸세요 ㅎㅎ
-
 if __name__ == "__main__":
 
     fullDataset = ImgDataset(CFG.trainPath, CFG.transform)
-    # valDataset = ImgDataset(CFG.valPath, CFG.transform)
     train_size, val_size = 30000, 7500
     # 0.8:0.2 입니다.
     trainDataset, valDataset = random_split(fullDataset, [train_size, val_size])
@@ -223,7 +179,6 @@ if __name__ == "__main__":
     optimizer = optim.Adam(autoencoder_params, lr=CFG.lr)
 
     val_loss_arr = [1e+10]
-    # loss값 저장을위해 trash value 한번해줌
 
     for epoch in range(1, CFG.epochs+1):
         train_one_epoch(encodeModel, decodeModel, optimizer, trainDataloader, epoch, CFG.loss, CFG.device)
@@ -233,8 +188,7 @@ if __name__ == "__main__":
     # 이거 어려울거 없이 그냥 embedding_dimension임 -> 사이즈 변경시 변경이 필요할 수 있음 -> 이거 쓰레기값으로 들어가는거라서
     # 딱히 상관은 없음 그러나 (1, c, h, w) -> 이런식으로 들어가네용
 
-
-
-
-
-
+    
+    # https://velog.io/@ppyooy336/Numpy%EC%9D%84-%ED%99%9C%EC%9A%A9%ED%95%98%EC%97%AC-KNN-%EA%B5%AC%ED%98%84
+    # 여기참조하고 https://github.com/public-ai/dl-lecture/blob/master/ml-homework/1_Numpy%EB%A5%BC%20%ED%99%9C%EC%9A%A9%ED%95%9C%20%EB%A8%B8%EC%8B%A0%EB%9F%AC%EB%8B%9D%20-%20KNN%20%EB%B6%84%EB%A5%98%EA%B8%B0.ipynb
+    # 이사람이 배운곳
--- test.py
+++ test.py
@@ -1,102 +1,114 @@
-import torch
+# import torch
 
-import numpy as np
+# import numpy as np
 
-from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+# from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
 
-class CFG:
-    in_channels = [3, 16, 32, 64, 128]
-    out_channels = [16, 32, 64, 128, 256]
-
-
-# for in_channel, out_channel in zip(CFG.in_channels, CFG.out_channels):
-#     print(in_channel)
-#     print(out_channel)
-
-# # [in_channel, out_channel for in_channel, out_channel in zip(CFG.in_channels, CFG.out_channels)]
+# class CFG:
+#     in_channels = [3, 16, 32, 64, 128]
+#     out_channels = [16, 32, 64, 128, 256]
 
 
-# print(list(reversed(CFG.in_channels)))
+# # for in_channel, out_channel in zip(CFG.in_channels, CFG.out_channels):
+# #     print(in_channel)
+# #     print(out_channel)
+
+# # # [in_channel, out_channel for in_channel, out_channel in zip(CFG.in_channels, CFG.out_channels)]
 
 
-
-# print([1,2,3,4,5]+[1,1,1,1,1,5,3,6,1,2])
-# 뒤로 배열됨.
+# # print(list(reversed(CFG.in_channels)))
 
 
 
-# print(torch.randn(100).size())
+# # print([1,2,3,4,5]+[1,1,1,1,1,5,3,6,1,2])
+# # 뒤로 배열됨.
 
 
-# arr = [1,2,3,4,5,6]
 
-# print(max(arr[:-1]))
-
-# a = torch.randn(1,3,512,512)
-# b = torch.randn(1,3,512,512)
+# # print(torch.randn(100).size())
 
 
-# print(a.size())
+# # arr = [1,2,3,4,5,6]
 
-# k = torch.cat((a,b), 0)
-# print(k.size())
-all_dataset = np.array([[1,2,3,4,6,7,10,8,8,4],
-                [1,2,3,2,6,7,8,2,8,4],
-                [1,2,2,3,6,7,2,8,6,2],
-                [1,2,3,3,6,4,4,2,2,6],
-                [1,2,5,4,6,7,8,2,8,1],
-                [1,2,3,3,6,1,8,4,8,5]])
+# # print(max(arr[:-1]))
 
-image = np.array([5,12,3,2,4,6,2,4,5,6])
-
-# a3 = (a1-a2)**2
-# print(np.sqrt(a3.sum(axis=1)))
+# # a = torch.randn(1,3,512,512)
+# # b = torch.randn(1,3,512,512)
 
 
-# 정규화 + 후에 np.sqrt(k^2)
-# 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
+# # print(a.size())
 
+# # k = torch.cat((a,b), 0)
+# # print(k.size())
+# all_dataset = np.array([[1,2,3,4,6,7,10,8,8,4],
+#                 [1,2,3,2,6,7,8,2,8,4],
+#                 [1,2,2,3,6,7,2,8,6,2],
+#                 [1,2,3,3,6,4,4,2,2,6],
+#                 [1,2,5,4,6,7,8,2,8,1],
+#                 [1,2,3,3,6,1,8,4,8,5]])
+
+# image = np.array([5,12,3,2,4,6,2,4,5,6])
+
+# # a3 = (a1-a2)**2
+# # print(np.sqrt(a3.sum(axis=1)))
+
+
+# # 정규화 + 후에 np.sqrt(k^2)
+# # 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
+
+
+# # def k_nearest_neighbor(image, all_dataset, labels, K):
+# #     knn = NearestNeighbors(n_neighbors=2, metric='cosine')
+# #     knn.fit(image)
+# #     # 필요시 axis 바꾸세요 ㅎㅎ
+
+# #     _, indices = knn.kneighbors(all_dataset)
+# #     indices_list = indices.tolist()
+# #     print(f'indices_list:{indices_list}')
 
 # def k_nearest_neighbor(image, all_dataset, labels, K):
-#     knn = NearestNeighbors(n_neighbors=2, metric='cosine')
-#     knn.fit(image)
-#     # 필요시 axis 바꾸세요 ㅎㅎ
+#     # 정규화 + 후에 np.sqrt(k^2)
+#     # 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
+#     square = (all_dataset-image)**2
 
-#     _, indices = knn.kneighbors(all_dataset)
-#     indices_list = indices.tolist()
-#     print(f'indices_list:{indices_list}')
+#     distance = np.sqrt(square.sum(axis=1))
+#     # print(distance)
+#     # print(distance.shape)
 
-def k_nearest_neighbor(image, all_dataset, labels, K):
-    # 정규화 + 후에 np.sqrt(k^2)
-    # 정규화를 하는 대신에 z 표준화를 하는방식이 제일 많이쓰인답니다.
-    square = (all_dataset-image)**2
+#     sorted_idx = np.argsort(distance)[:K]
+#     print(sorted_idx)
+#     # 제일작은 index를 반환함
 
-    distance = np.sqrt(square.sum(axis=1))
-    # print(distance)
-    # print(distance.shape)
+#     for i in sorted_idx:
+#         similarImgPath = labels[i]
+#         print(similarImgPath)
 
-    sorted_idx = np.argsort(distance)[:K]
-    print(sorted_idx)
-    # 제일작은 index를 반환함
+# labels = ['./dataset/mask1.jpg', './dataset/123.jpg', './dataset/634125.jpg', './dataset/162.jpg', './dataset/6131.jpg', './dataset/63127.jpg']
 
-    for i in sorted_idx:
-        similarImgPath = labels[i]
-        print(similarImgPath)
-
-labels = ['./dataset/mask1.jpg', './dataset/123.jpg', './dataset/634125.jpg', './dataset/162.jpg', './dataset/6131.jpg', './dataset/63127.jpg']
-
-# 이런식으로 우선 개발진행해보자
-k_nearest_neighbor(image, all_dataset, labels, K=5)
+# # 이런식으로 우선 개발진행해보자
+# k_nearest_neighbor(image, all_dataset, labels, K=5)
 
 
-# embedding을 이해하려면 이거 돌려보면됨.  reshape안해도 괜찮긌네
-# k = torch.randn(2,1,3,3)
+# # embedding을 이해하려면 이거 돌려보면됨.  reshape안해도 괜찮긌네
+# # k = torch.randn(2,1,3,3)
 
-# print(f'k:{k}')
-# print(f'k:{k.size()}')
+# # print(f'k:{k}')
+# # print(f'k:{k.size()}')
 
 
-# m = k.flatten(start_dim=1)
-# print(m)
-# print(m.size())
+# # m = k.flatten(start_dim=1)
+# # print(m)
+# # print(m.size())
 
+import torch
+
+import timm
+
+# print(timm.list_models(pretrained=True))
+model = timm.create_model('efficientnet_b0')
+print(model)
+model.global_pool = torch.nn.Identity()
+model.classifier = torch.nn.Identity()
+random = torch.randn(1,3,224,224)
+
+print(model(random).size())
